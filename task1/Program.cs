3e66cb12a0db99b7e7d5be111353322c3ec56e56
ilnur-Git <mazel08@yandex.ru>
2023-02-10 23:21:58 +0300

    55

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 == number2 * number2)
+    {
+    Console.Write("Да");
+    }
+    else 
+    {
+    Console.Write("Нет");
+    }
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,31 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите день недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
-if (day == 1);
+if (day == 1)
 {
     Console.Write("Понедельник");
 }
-else if (day == 2);
+else if (day == 2)
 {
     Console.Write("Вторник");
 }
-else if (day == 3);
+else if (day == 3)
 {
     Console.Write("среда");
 }
-else if (day == 4);
+else if (day == 4)
 {
     Console.Write("четверг");
 }
-else if (day == 5);
+else if (day == 5)
 {
     Console.Write("пятница");
 }
-else if (day == );
+else if (day == 6)
 {
     Console.Write("суббота");
 }
-else if (day == 7);
+else if (day == 7)
 {
     Console.Write("воскресенье");
 }
