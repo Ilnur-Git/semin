cb049fe5ed4e8d55c880f75b23ea20403a56eae7
ilnur-Git <mazel08@yandex.ru>
2023-02-10 22:58:27 +0300

    55

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1);
+{
+    Console.Write("Понедельник");
+}
+else if (day == 2);
+{
+    Console.Write("Вторник");
+}
+else if (day == 3);
+{
+    Console.Write("среда");
+}
+else if (day == 4);
+{
+    Console.Write("четверг");
+}
+else if (day == 5);
+{
+    Console.Write("пятница");
+}
+else if (day == );
+{
+    Console.Write("суббота");
+}
+else if (day == 7);
+{
+    Console.Write("воскресенье");
+}
+else
+{
+    Console.Write("Такого дня недели нет");
+}
